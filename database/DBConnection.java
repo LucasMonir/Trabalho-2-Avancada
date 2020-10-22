4c2846179e320f3682c45711e798e51188f54743
Ian <iankkles@gmail.com>
2020-10-21 22:34:19 -0300

    Criado o arquivo de Conexão do BD.

---
+++ database/DBConnection.java
@@ -0,0 +1,36 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    static private Connection connection = null;
+    static private String url = "";
+    static private String username = "";
+    static private String password = "";
+    static private String dbdriver = "";
+
+    static public void setConfig(String iurl, String uname, String pass, String dbdriv) {
+        url = iurl;
+        username = uname;
+        password = pass;
+        dbdriver = dbdriv;
+    }
+
+    static public Connection getConnection() {
+        if (connection = null) {
+            try {
+
+                Class.forName(dbdriver);
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return connection;
+    }
+}
